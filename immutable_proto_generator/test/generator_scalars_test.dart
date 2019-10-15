142515448e3e4e204dd388f13f5be35fabab6256
Jonas Wanke <contact@jonas-wanke.com>
2019-10-15 10:08:34 +0200

    feat(generator): add more tests

--- immutable_proto_generator/test/generator_scalars_test.dart
+++ immutable_proto_generator/test/generator_scalars_test.dart
@@ -16,6 +16,106 @@ class MutableScalarsMessage {}
 
 Future<void> main() async {
   group('ScalarsMessage', () {
+    final messageWithNullsBuilder = () => ScalarsMessage(
+          double_1: null,
+          float: null,
+          int32: null,
+          int64: null,
+          uint32: null,
+          uint64: null,
+          sint32: null,
+          sint64: null,
+          fixed32: null,
+          fixed64: null,
+          sfixed32: null,
+          sfixed64: null,
+          bool_13: null,
+          string: null,
+          bytes: KtList.empty(),
+        );
+    final messageWithZerosBuilder = () => ScalarsMessage(
+          double_1: 0.0,
+          float: 0.0,
+          int32: 0,
+          int64: Int64(0),
+          uint32: 0,
+          uint64: Int64(0),
+          sint32: 0,
+          sint64: Int64(0),
+          fixed32: 0,
+          fixed64: Int64(0),
+          sfixed32: 0,
+          sfixed64: Int64(0),
+          bool_13: false,
+          string: '',
+          bytes: KtList.empty(),
+        );
+
+    group('.fromProto', () {
+      test('works with null values', () {
+        expect(messageWithNullsBuilder, returnsNormally);
+      });
+      test('crashes with null bytes', () {
+        expect(() => ScalarsMessage(bytes: null),
+            throwsA(TypeMatcher<AssertionError>()));
+      });
+      test('works with zero values', () {
+        expect(messageWithZerosBuilder, returnsNormally);
+      });
+    });
+    group('.toProto', () {
+      group('with null values', () {
+        test('returns normally', () {
+          expect(() => messageWithNullsBuilder().toProto(), returnsNormally);
+        });
+        test('generates correct protobuf', () {
+          expect(
+              messageWithNullsBuilder().toProto(),
+              isA<proto.ScalarsMessage>()
+                  .having((m) => m.hasDouble_1(), 'double', isFalse)
+                  .having((m) => m.hasFloat(), 'float', isFalse)
+                  .having((m) => m.hasInt32(), 'int32', isFalse)
+                  .having((m) => m.hasInt64(), 'int64', isFalse)
+                  .having((m) => m.hasUint32(), 'uint32', isFalse)
+                  .having((m) => m.hasUint64(), 'uint64', isFalse)
+                  .having((m) => m.hasSint32(), 'sint32', isFalse)
+                  .having((m) => m.hasSint64(), 'sint64', isFalse)
+                  .having((m) => m.hasFixed32(), 'fixed32', isFalse)
+                  .having((m) => m.hasFixed64(), 'fixed64', isFalse)
+                  .having((m) => m.hasSfixed32(), 'sfixed32', isFalse)
+                  .having((m) => m.hasSfixed64(), 'sfixed64', isFalse)
+                  .having((m) => m.hasBool_13(), 'bool_13', isFalse)
+                  .having((m) => m.hasString(), 'string', isFalse)
+                  .having((m) => m.hasBytes(), 'bytes', isFalse));
+        });
+      });
+      group('with zero values', () {
+        test('returns normally', () {
+          expect(() => messageWithZerosBuilder().toProto(), returnsNormally);
+        });
+        test('generates correct protobuf', () {
+          expect(
+              messageWithZerosBuilder().toProto(),
+              isA<proto.ScalarsMessage>()
+                  .having((m) => m.hasDouble_1(), 'double', isTrue)
+                  .having((m) => m.hasFloat(), 'float', isTrue)
+                  .having((m) => m.hasInt32(), 'int32', isTrue)
+                  .having((m) => m.hasInt64(), 'int64', isTrue)
+                  .having((m) => m.hasUint32(), 'uint32', isTrue)
+                  .having((m) => m.hasUint64(), 'uint64', isTrue)
+                  .having((m) => m.hasSint32(), 'sint32', isTrue)
+                  .having((m) => m.hasSint64(), 'sint64', isTrue)
+                  .having((m) => m.hasFixed32(), 'fixed32', isTrue)
+                  .having((m) => m.hasFixed64(), 'fixed64', isTrue)
+                  .having((m) => m.hasSfixed32(), 'sfixed32', isTrue)
+                  .having((m) => m.hasSfixed64(), 'sfixed64', isTrue)
+                  .having((m) => m.hasBool_13(), 'bool_13', isTrue)
+                  .having((m) => m.hasString(), 'string', isTrue)
+                  .having((m) => m.hasBytes(), 'bytes', isFalse));
+        });
+      });
+    });
+
     group('has correct generated field types:', () {
       const types = {
         #double_1: double,
@@ -30,6 +130,10 @@ Future<void> main() async {
         #fixed64: Int64,
         #sfixed32: int,
         #sfixed64: Int64,
+        #bool_13: bool,
+        #string: String,
+        // Not testing bytes yet due to generics
+        // #bytes: KtList,
       };
 
       final messageClass = reflectClass(ScalarsMessage);
